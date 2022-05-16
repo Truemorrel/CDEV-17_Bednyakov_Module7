51eacc034ef4524394f3c291decd80888591f458
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-17 01:21:25 +0300

    master: add task 7.1.5

---
+++ Task_7_1_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task_7_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Herbal
+        {
+            private string PlantName
+            {
+                get { return PlantName; }
+                set { PlantName = value; }  
+            }
+        }
+        class Fruite : Herbal
+        {
+            private string Name
+            {
+                get { return Name; }
+                set { Name = value; }
+            }
+        }
+        class Vegetable : Herbal
+        {
+            private string Name
+            {
+                get
+                {
+                    return Name;
+                }
+                set
+                {
+                    Name = value;
+                }
+            }
+        }
+        class Apple : Fruite { }
+        class Banana : Fruite { }
+        class Pear : Fruite { }
+        class Potato : Vegetable { }
+        class Carrot : Vegetable { }
+    }
+}
