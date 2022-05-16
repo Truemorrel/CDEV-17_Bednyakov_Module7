b578876e7139788bf0b5bf924ea68ba77e4ee2dd
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-16 23:51:52 +0300

    master: add task 7.1.5

---
+++ Task_7_1_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_7_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //ProjectManager и Developer
+        class Employee
+        {
+            public string Name;
+            public int Age;
+            public int Salary;
+        }
+        class ProjectManager : Employee
+        {
+            public string ProjectName;
+        }
+        class Developer : Employee
+        {
+            public string ProgrammingLanguage;
+        }
+    }
+}
