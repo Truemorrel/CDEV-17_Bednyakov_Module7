18562b55e31fb29c536ab30ee7219508a1ac2315
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-17 22:38:48 +0300

    master: update task 7.1.10

---
+++ Task_7_1_10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_7_1_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class BaseClass
+        {
+            protected string Name;
+
+            public BaseClass(string name)
+            {
+                Name = name;
+            }
+        }
+
+        class DerivedClass : BaseClass
+        {
+            public string Description;
+
+            public int Counter;
+            public DerivedClass(string name, string Description) : base(name) 
+            {
+                this.Description = Description;
+            }
+            public DerivedClass(string name, string Description, int Counter) : this(name, Description)   
+            {
+                this.Counter = Counter;
+            }
+        }
+    }
+}
