910f8bb21702955cd1ea6f98ff6fde92e1379921
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 02:26:00 +0300

    master: add task 7.2.12

---
+++ Task_7_2_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_7_2_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Obj
+        {
+            public int Value;
+
+            public static Obj operator + (Obj a, Obj b)
+            {
+                return new Obj
+                {
+                    Value = a.Value + b.Value
+                };
+            }
+            public static Obj operator - (Obj a, Obj b)
+            {
+                return new Obj
+                {
+                    Value = a.Value - b.Value
+                };
+            }
+        }
+    }
+}
