5c287c3e81a0e18a20f9bfa9042d74dad9ef765e
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 04:59:26 +0300

    master: add task 7.5.5

---
+++ Task_7_5_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_7_5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Obj
+        {
+            public string Name;
+            public string Description;
+
+            public static string Parent;
+            public static int DaysInWeek;
+            public static int MaxValue;
+            static Obj()
+            {
+                Parent = "System.Object";
+                DaysInWeek = 7;
+                MaxValue = 2000;
+            }
+        }
+    }
+}
