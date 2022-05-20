363c5398a1d741342903976835989ba2f3d18e8f
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 14:12:18 +0300

    master: add task 7.5.9

---
+++ Task_7_5_9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_7_5_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+            int num2 = -13;
+            int num3 = 0;
+
+            Console.WriteLine(num1.GetNegative()); //-7
+            Console.WriteLine(num1.GetPositive()); //7
+            Console.WriteLine(num2.GetNegative()); //-13
+            Console.WriteLine(num2.GetPositive()); //13
+            Console.WriteLine(num3.GetNegative()); //0
+            Console.WriteLine(num3.GetPositive()); //0
+        }
+        
+    }
+    static class intExtensions
+    {
+        public static int GetNegative(this int num)
+        {
+            if(num < 0)
+            {
+                return num;
+            }
+            else
+            {
+                return -num;
+            }
+        }
+        public static int GetPositive(this int num)
+        {
+            if (num >= 0)
+            {
+                return num;
+            }
+            else
+            {
+                return -num;
+            }
+        }
+    }
+
+}
