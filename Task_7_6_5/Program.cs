f4bb4a6c9edb527b6f75d7fc1fdd425d9b6a5fcd
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 21:33:21 +0300

    master: add task 7.6.5.

---
+++ Task_7_6_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_7_6_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Record <T1, T2>
+        {
+            public T1 Id;
+            public DateTime Date;
+            public T2 Value;
+        }
+    }
+}
