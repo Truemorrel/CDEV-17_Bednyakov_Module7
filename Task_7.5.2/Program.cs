fa84d46204c084d5a6bdc330ef0de95a94931357
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 04:45:08 +0300

    master: add task 7.5.2

---
+++ Task_7.5.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_7._5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Obj
+        {
+            public string Name;
+            public string Description;
+            public static int MaxValue = 2000;
+        }
+    }
+}
