abb9254231577b3508162f862320e65ea45d3c40
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 04:43:06 +0300

    master: add task 7.5.3

---
+++ Task_7_5_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_7_5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1,ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
+        }
+        class Helper
+        {
+            public static int Sum(int a, int b, int c)
+            {
+                return a + b + c;
+            }
+            public static void Swap(ref int a, ref int b)
+            {
+                var c = a;
+                a = b;
+                b = c; 
+            }
+        }
+    }
+}
