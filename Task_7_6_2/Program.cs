6b71d0ed59354e95b811c12978de8a308f471a39
Sergey Bednyakov <sbednyakov@gmail.com>
2022-05-20 18:28:43 +0300

    master: add task 7.6.2.

---
+++ Task_7_6_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_7_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Car <T>
+        {
+            public T engine;
+        }
+        class ElectricEngine { }
+        class GasEngine { }
+    }
+}
